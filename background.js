22e987883384a6d224f91e16547627f5a5645041
LZ27@proton.me <157941859+Newfies@users.noreply.github.com>
2024-09-19 12:43:15 -0500

    Making Menus and Sub Items!

--- background.js
+++ background.js
@@ -1,14 +1,25 @@
 chrome.runtime.onInstalled.addListener(() => {
+    // YouTube Menu
     chrome.contextMenus.create({
         id: "ytContextMenu",
-        title: "YouTube Video To No Cookie",
+        title: "[🌐] YouTube",
         contexts: ["all"],
         visible: false
     });
 
     chrome.contextMenus.create({
+        id: "ytNoCookie",
+        title: "NoCookie-ify Video",
+        parentId: "ytContextMenu",  // Sub Item
+        contexts: ["all"],
+        visible: false
+    });
+
+    // Default Menu
+
+    chrome.contextMenus.create({
         id: "otherContextMenu",
-        title: "Other Site Menu Item",
+        title: "Misc",
         contexts: ["all"],
         visible: false
     });
@@ -19,22 +30,21 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (changeInfo.status === 'complete' && tab.url) {
         const url = tab.url;
 
-        // Set visibility based on the website
+        // Enabling Based On Website
         if (url.includes("youtube.com")) {
             chrome.contextMenus.update("ytContextMenu", { visible: true });
-            chrome.contextMenus.update("otherContextMenu", { visible: true });
-        } /* else {
-            chrome.contextMenus.update("ytContextMenu", { visible: false });
-            chrome.contextMenus.update("otherContextMenu", { visible: true });
-        } */
+            chrome.contextMenus.update("ytNoCookie", { visible: true });
+        }
+        // Enabling These By Default
+        chrome.contextMenus.update("otherContextMenu", { visible: true });
     }
 });
 
 // Handle context menu item clicks
 chrome.contextMenus.onClicked.addListener((info, tab) => {
-    if (info.menuItemId === "ytContextMenu") {
-        chrome.tabs.executeScript(tab.id, { file: 'CS/YT/ytScript.js' });
+    if (info.menuItemId === "ytNoCookie") {
+        chrome.tabs.executeScript(tab.id, { file: 'CS/YT/ytNoCookie.js' });
     } else if (info.menuItemId === "otherContextMenu") {
-        console.log("Other site menu item clicked");
+        chrome.tabs.executeScript(tab.id, { file: 'CS/General/Script1.js' });
     }
 });
